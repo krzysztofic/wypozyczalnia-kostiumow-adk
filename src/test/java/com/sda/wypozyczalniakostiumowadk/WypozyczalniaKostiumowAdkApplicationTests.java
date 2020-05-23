3aab61df2b8b3b2a75ebac7e3cf3530d27030441
Damian Bukowski <damianbukowski@email.com>
2020-05-23 10:23:12 +0200

    Initial commit

---
+++ src/test/java/com/sda/wypozyczalniakostiumowadk/WypozyczalniaKostiumowAdkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sda.wypozyczalniakostiumowadk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WypozyczalniaKostiumowAdkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
