296bc0fdbece1f3a4da8a4e00bb5212b8bd7210e
Damian Bukowski <damianbukowski@email.com>
2020-07-11 18:05:25 +0200

    searchbar merged with master, search function do not fully work atm

--- src/main/java/com/sda/repository/CostumeRepository.java
+++ src/main/java/com/sda/repository/CostumeRepository.java
@@ -1,13 +1,14 @@
 package com.sda.repository;
 
 import com.sda.model.Costume;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
-import java.util.Set;
+import java.util.Optional;
 
-public interface CostumeRepository extends JpaRepository<Costume, Long> {
+public interface CostumeRepository extends PagingAndSortingRepository<Costume, Long> {
 
+    Costume getOne(Long id);
     List<Costume> findByNameLikeIgnoreCase(String name);
 }
 
--- src/main/java/com/sda/service/OrderService.java
+++ src/main/java/com/sda/service/OrderService.java
@@ -2,25 +2,61 @@ package com.sda.service;
 
 import com.sda.model.Costume;
 import com.sda.repository.CostumeRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Optional;
 import java.util.List;
 
 @Service
 public class OrderService {
 
-    private final CostumeRepository costumeRepository;
+    private CostumeRepository costumeRepository;
 
     public OrderService(CostumeRepository costumeRepository) {
         this.costumeRepository = costumeRepository;
     }
-    public List<Costume> findAll() {
-        return costumeRepository.findAll();
+
+    public Page<Costume> findAll(PageRequest page) {
+        return costumeRepository.findAll(page);
     }
+
     public List<Costume> listAll() {
         List<Costume> costumes = new ArrayList<>();
         costumeRepository.findAll().forEach(costumes::add);
         return costumes;
+    }
+
+    public Costume getOne(Long id) {
+        return costumeRepository.getOne(id);
+    }
+
+    public Costume add(Costume costume) {
+        return costumeRepository.save(costume);
+    }
+
+    public void remove(Long id) {
+        costumeRepository.delete(costumeRepository.findById(id).get());
+    }
+
+    public Optional<Costume> borrowCostume(Long id) {
+        Optional<Costume> notBorrowedCostume = costumeRepository.findById(id).stream()
+                .filter(costume -> costume.getBorrowedTill() == null).findAny();
+        if (notBorrowedCostume.isPresent()) {
+            Costume costumeToBorrow = notBorrowedCostume.get();
+            costumeToBorrow.setBorrowedTill(LocalDate.now().plusDays(7));
+            return Optional.of(costumeRepository.save(costumeToBorrow));
+        }
+        return notBorrowedCostume;
+    }
+
+    public Costume returnCostume(Long id) {
+        Costume costumeToReturn = costumeRepository.findById(id).get();
+        costumeToReturn.setBorrowedTill(null);
+        return costumeRepository.save(costumeToReturn);
     }
 
     public List<Costume> findByName (String name) {
