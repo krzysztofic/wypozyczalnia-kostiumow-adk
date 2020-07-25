7b8a3528a5c3c7b480b5d548022d478130016cea
Krzysztof Iciek <Krzysztof.iciek@gmail.com>
2020-07-25 10:43:56 +0200

    Booking (#12)
    * Added Email configuration and modified Customer. Also added propoerties to mailtrap to be able to send orders by emial
    
    * Modyfied cotroller  and added propoerties to send an email from contact form. Messages are available on mailtrap
    
    * Changed styles in rentForm and feedback template
    
    * Fixed login form broken by my last chenages from contact form.

--- src/main/java/com/sda/controller/FormController.java
+++ src/main/java/com/sda/controller/FormController.java
@@ -1,13 +1,25 @@
 package com.sda.controller;
 
+import com.sda.config.EmailConfig;
 import com.sda.model.Customer;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.xml.bind.ValidationException;
+
 @Controller
 public class FormController {
+
+    private EmailConfig emailConfig;
+
+    public FormController(EmailConfig emailConfig) {
+        this.emailConfig = emailConfig;
+    }
 
     @GetMapping("/rentForm")
     public String sendForm(Customer customer) {
@@ -15,7 +27,26 @@ public class FormController {
     }
 
     @PostMapping("/rentForm")
-    public String handleCostumeForm(@ModelAttribute Customer customer) {
+    public String handleCostumeForm(@ModelAttribute Customer customer,
+                                    BindingResult bindingResult) throws ValidationException {
+        if (bindingResult.hasErrors()) {
+            throw new ValidationException("Formularz jest niepoprawny!");
+        }
+
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(this.emailConfig.getHost());
+        mailSender.setPort(this.emailConfig.getPort());
+        mailSender.setUsername(this.emailConfig.getUsername());
+        mailSender.setPassword(this.emailConfig.getPassword());
+
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setFrom(customer.getEmail());
+        mailMessage.setTo("biuro@adk.pl");
+        mailMessage.setSubject("Nowe zamówienie od "+ customer.getName() + "!");
+        mailMessage.setText(customer.getMessage());
+
+        mailSender.send(mailMessage);
+
         return "thank-you";
     }
 }
